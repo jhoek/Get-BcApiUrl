8fa787d85228e59d75d61537a36a0ce2a61bf705
Jan Hoek <github@uncommonsense.nl>
2025-07-23 09:43:40 +0200

    Preparations

--- Get-BcApiUrl.Tests.ps1
+++ Get-BcApiUrl.Tests.ps1
@@ -1,6 +1,20 @@
+# API (Beta)	        https://{businesscentralPrefix}/v2.0/{Tenant id}/{Environment id}/api/{APIPublisher}/{APIGroup}/beta/companies({id})/
+# API (v1.0)	        https://{businesscentralPrefix}/v2.0/{Tenant id}/{Environment id}/api/v1.0/companies({id})/
+# API (v2.0)	        https://{businesscentralPrefix}/v2.0/{Tenant id}/{Environment id}/api/v2.0/companies({id})/
+# custom APIs	        https://{businesscentralPrefix}/v2.0/{Tenant id}/{Environment id}/api/{APIPublisher}/{APIGroup}/v2.0/companies({id})/{EntitySetName}
+# automation APIs	    https://{businesscentralPrefix}/v2.0/{Tenant id}/{Environment id}/api/microsoft/automation/v2.0/companies({id})
+# OData Web Services	https://{businesscentralPrefix}/v2.0/{Tenant id}/{Environment id}/ODataV4/companies({name})/{Service Name}
+
 Describe 'Get-BcApiUrl' {
 	Context 'Get-BcApiUrl' {
-		It 'works' {
+        BeforeAll {
+            $TenantId = New-Guid
+            $CompanyId = New-Guid
+            $Environment = 'My Environment'
+        }
+
+		It 'Standard Beta' {
+            Get-BcApiUrl -StandardBeta -TenantId $TenantId -Environment $Environment -CompanyId $CompanyId
 		}
 	}
 }
--- Public/Get-BcApiUrl.ps1
+++ Public/Get-BcApiUrl.ps1
@@ -1,6 +1,52 @@
+# https://yzhums.com/52262
 function Get-BcApiUrl
 {
+    [CmdletBinding(DefaultParameterSetName = 'Custom')]
     param
     (
+        [Parameter(ParameterSetName = 'StandardBeta')]
+        [switch]$StandardBeta,
+
+        [Parameter(ParameterSetName = 'Standard')]
+        [switch]$Standard,
+
+        [Parameter(ParameterSetName = 'Custom')]
+        [switch]$Custom,
+
+        [Parameter(ParameterSetName = 'Automation')]
+        [switch]$Automation,
+
+        [Parameter(ParameterSetName = 'ODataWebServices')]
+        [switch]$ODataWebservices,
+
+        [Parameter(Mandatory)]
+        [guid]$TenantId,
+
+        [Parameter(Mandatory)]
+        [string]$Environment,
+
+        [Parameter(ParameterSetName = 'Standard')]
+        [ValidateNotNullOrEmpty()]
+        [string]$StandardVersion = 'v2.0',
+
+        [Parameter(Mandatory, ParameterSetName = 'StandardBeta')]
+        [Parameter(Mandatory, ParameterSetName = 'Standard')]
+        [Parameter(Mandatory, ParameterSetName = 'Custom')]
+        [Parameter(Mandatory, ParameterSetName = 'Automation')]
+        [guid]$CompanyId,
+
+        [Parameter(Mandatory, ParameterSetName = 'ODataWebServices')]
+        [string]$CompanyName
     )
+
+    @(
+        'https://api.businesscentral.dynamics.com/v2.0',
+        $TenantId,
+        $Environment,
+        $ODataWebservices ? 'ODataV4' : 'api',
+        $Standard ? $StandardVersion : $null,
+        $ODataWebservices ? $null : "companies($CompanyId)"
+    )
+    | Where-Object { $_ }
+    | Join-String -Separator '/'
 }
